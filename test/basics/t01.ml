a27068b6454afcd6e2e3f261bd494a98faa48472
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-07-05 17:14:53 +0200

    Add a simple test about condition

---
+++ test/basics/t01.ml
@@ -0,0 +1,38 @@
+module Box = struct
+  type 'a t = { value: 'a option Atomic.t; lock: Mutex.t * Condition.t }
+
+  let push t value =
+    Mutex.lock (fst t.lock);
+    let set = Atomic.compare_and_set t.value None (Some value) in
+    Condition.broadcast (snd t.lock);
+    Mutex.unlock (fst t.lock);
+    set
+
+  let take t =
+    Mutex.lock (fst t.lock);
+    while Option.is_none (Atomic.get t.value) do
+      Condition.wait (snd t.lock) (fst t.lock)
+    done;
+    let value = Option.get (Atomic.get t.value) in
+    Mutex.unlock (fst t.lock);
+    value
+
+  let make () =
+    { value= Atomic.make None; lock= (Mutex.create (), Condition.create ()) }
+end
+
+open Miou
+
+let prgm () =
+  let box = Box.make () in
+  let p0 =
+    Prm.call @@ fun () ->
+    Miouu.sleep 1.;
+    assert (Box.push box "Hello World!" = true)
+  in
+  let p1 = Prm.call @@ fun () -> ignore (Box.take box) in
+  let p2 = Prm.call @@ fun () -> ignore (Box.take box) in
+  Prm.await_all [ p0; p1; p2 ]
+  |> List.iter @@ function Error exn -> raise exn | Ok () -> ()
+
+let () = Miouu.run prgm
---
+++ test/basics/t02.ml
@@ -0,0 +1,14 @@
+open Miou
+
+let () =
+  Miouu.run @@ fun () ->
+  let v = Atomic.make None in
+  let t = Miouu.Cond.make () in
+  let p0 = Prm.call @@ fun () -> Atomic.set v (Some ()); Miouu.Cond.signal t in
+  let p1 =
+    Prm.call @@ fun () ->
+    Miouu.Cond.wait t;
+    match Atomic.get v with Some v -> v | None -> failwith "p1"
+  in
+  Prm.await_all [ p0; p1 ]
+  |> List.iter (function Error exn -> raise exn | Ok () -> ())
