eb9c1632ee0c9d58369f7cd0b16a995b65368861
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-10-01 13:15:55 +0200

    Improve and finish the happy-eyeballs impl.

--- examples/happy/main.ml
+++ examples/happy/main.ml
@@ -16,7 +16,8 @@ let reporter ppf =
 
 let () = Fmt_tty.setup_std_outputs ~style_renderer:`Ansi_tty ~utf_8:true ()
 let () = Logs.set_reporter (reporter Fmt.stderr)
-let () = Logs.set_level ~all:true (Some Logs.Debug)
+
+(* let () = Logs.set_level ~all:true (Some Logs.Debug) *)
 let () = Logs_threaded.enable ()
 let () = Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna)
 let out = Mutex.create ()
@@ -35,14 +36,17 @@ let pp_msg ppf (`Msg msg) = Fmt.string ppf msg
 let osau_re = Domain_name.(host_exn (of_string_exn "osau.re"))
 let google_com = Domain_name.(host_exn (of_string_exn "google.com"))
 
+let getaddrinfo dns =
+  { Happy.getaddrinfo= (fun record host -> Mdns.getaddrinfo dns record host) }
+
 let () =
   Miou_unix.run @@ fun () ->
-  let daemon, stack = Happy.daemon ~timeout:(Duration.of_sec 1) in
-  let dns = Mdns.create ~cache_size:1 stack in
-  for _ = 0 to 10_0000 do
-    let res = Mdns.gethostbyname dns osau_re in
-    pr "%a\n%!" Fmt.(Dump.result ~ok:Ipaddr.V4.pp ~error:pp_msg) res;
-    let res = Mdns.gethostbyname dns google_com in
-    pr "%a\n%!" Fmt.(Dump.result ~ok:Ipaddr.V4.pp ~error:pp_msg) res
+  let daemon, stack = Happy.stack () in
+  let dns = Mdns.create stack in
+  Happy.inject_resolver ~getaddrinfo:(getaddrinfo dns) stack;
+  for i = 0 to 10_000 do
+    match Happy.connect_endpoint stack "google.com" [ 443 ] with
+    | Ok (_, fd) -> Miou_unix.close fd
+    | Error (`Msg msg) -> failwith msg
   done;
   Happy.kill daemon
