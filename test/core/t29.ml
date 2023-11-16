895cd73ca5f60cc3ce9735cae5f07c9b2a45d73f
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-11-16 21:24:05 +0100

    Add some more tests

--- test/core/t29.ml
+++ test/core/t29.ml
@@ -1,4 +1,7 @@
 type _ Effect.t += Foo : unit Effect.t
 
 let () =
-  Miou.run @@ fun () -> try Effect.perform Foo with Effect.Unhandled Foo -> ()
+  try
+    Miou.run @@ fun () ->
+    try Effect.perform Foo with Effect.Unhandled Foo -> failwith "t29"
+  with Effect.Unhandled Foo -> ()
