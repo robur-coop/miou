6a29877a070b2666d8f92e57b774357d1a03928f
Calascibetta Romain <romain.calascibetta@gmail.com>
2025-10-08 11:30:18 +0200

    Change the license of miou_bitv.ml{,i} to MIT (from LGPL-2.1-or-later)
    Jean-Christophe Filliatre authorized us to change the license of miou_bitv.ml{,i}

--- lib/miou_bitv.ml
+++ lib/miou_bitv.ml
@@ -1,20 +1,5 @@
-(**************************************************************************)
-(*                                                                        *)
-(*  Copyright (C) Jean-Christophe Filliatre                               *)
-(*                                                                        *)
-(*  This software is free software; you can redistribute it and/or        *)
-(*  modify it under the terms of the GNU Library General Public           *)
-(*  License version 2, with the special exception on linking              *)
-(*  described in file LICENSE.                                            *)
-(*                                                                        *)
-(*  This software is distributed in the hope that it will be useful,      *)
-(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
-(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *)
-(*                                                                        *)
-(**************************************************************************)
-
 (* Copyright (c) Jean-Christophe Filliatre
-   SPDX-License-Identifier: LGPL-2.1-or-later
+   SPDX-License-Identifier: MIT
    Copyright (c) 2025 Romain Calascibetta <romain.calascibetta@gmail.com> *)
 
 (* Bit vectors. The interface and part of the code are borrowed from the
--- lib/miou_bitv.mli
+++ lib/miou_bitv.mli
@@ -1,18 +1,3 @@
-(**************************************************************************)
-(*                                                                        *)
-(*  Copyright (C) Jean-Christophe Filliatre                               *)
-(*                                                                        *)
-(*  This software is free software; you can redistribute it and/or        *)
-(*  modify it under the terms of the GNU Library General Public           *)
-(*  License version 2, with the special exception on linking              *)
-(*  described in file LICENSE.                                            *)
-(*                                                                        *)
-(*  This software is distributed in the hope that it will be useful,      *)
-(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
-(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *)
-(*                                                                        *)
-(**************************************************************************)
-
 (** This module implements bit vectors, as an abstract datatype [t]. Since bit
     vectors are particular cases of arrays, this module provides the same
     operations as module [Array]. It also provides bitwise operations and
